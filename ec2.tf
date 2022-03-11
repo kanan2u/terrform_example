e12dba5f3a09a43b651a01694d87798151326033
kanan2u <79785957+kanan2u@users.noreply.github.com>
2022-03-11 16:43:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAUJKSA52N3W4ZUOXF"
-  secret_key = "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"
+ 
 }
 resource "aws_instance" "myec2" {
   ami           = "ami-082b5a644766e0e6f"
